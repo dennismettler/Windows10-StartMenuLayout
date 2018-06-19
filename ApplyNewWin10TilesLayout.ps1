3fed26dfea5aee54bc49ce4cb89fefe8184364d9
Dennis Mettler <39314684+dennismettler@users.noreply.github.com>
2018-06-19 10:37:09 +0200

    Create ApplyNewWin10TilesLayout.ps1

---
+++ ApplyNewWin10TilesLayout.ps1
@@ -0,0 +1,10 @@
+# Copy The Customized XML File to the correct Destination
+Copy-Item -Path LayoutExport.xml -Destination %localappdata%\Microsoft\Windows\Shell\LayoutModification.xml -Force
+
+# Delete Every Registry Key in the Cache including "tiles"
+Get-ChildItem "HKCU:Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount"|
+    Where Name -like "*tile*"|
+        Remove-Item -Recurse -Force
+
+# Restart Explorer.exe
+Get-Process Explorer | Stop-Process
